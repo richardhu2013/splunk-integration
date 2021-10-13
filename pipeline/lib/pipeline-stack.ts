d8358d7383b95389661634f7aa97d03a6519c11e
Richard Hu <wenqing.hu@cba.com.au>
2021-10-13 13:35:26 +1100

    Add a command to ls src

--- pipeline/lib/pipeline-stack.ts
+++ pipeline/lib/pipeline-stack.ts
@@ -20,6 +20,7 @@ export class PipelineStack extends Stack {
               'pwd',
               'cd app/lambda',
               'pwd',
+              'ls src',
               'npm install'
             ],
           },
